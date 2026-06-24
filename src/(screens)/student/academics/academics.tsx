30a7cf34477977d6256f61de33f6ca0b6cb359aa
gkelite-info <gkeliteinfo@gmail.com>
2026-06-24 13:29:09 +0530

    solved issue with calendar

--- src/(screens)/student/academics/academics.tsx
+++ src/(screens)/student/academics/academics.tsx
@@ -44,54 +44,54 @@ function AcademicsContent() {
     paddingTop: headerHeight + 8,
     paddingBottom: isLargeScreen ? 20 : 100
   }} showsVerticalScrollIndicator={false} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />} alwaysBounceVertical={true} overScrollMode="always">
-      
-            <View className="flex-row justify-between items-center mb-5">
-                <View style={{
+
+    <View className="flex-row justify-between items-center mb-5">
+      <View style={{
         width: "68%"
       }} className="flex-col">
-                    <Text className="text-[#282828] text-[28px] mb-1" style={{
+        <Text className="text-[#282828] text-[28px] mb-1" style={{
           fontFamily: fonts.bold
         }}>
-                        {t("Academics")}
-                    </Text>
+          {t("Academicss")}
+        </Text>
 
-                    {isLargeScreen ? <Text className="text-[#282828] text-sm" style={{
+        {isLargeScreen ? <Text className="text-[#282828] text-sm" style={{
           fontFamily: fonts.regular
         }}>
-                            {t("Track syllabus progress and manage notes by semester")}
-                        </Text> : <Text className="text-[#282828] text-sm" style={{
+          {t("Track syllabus progress and manage notes by semester")}
+        </Text> : <Text className="text-[#282828] text-sm" style={{
           fontFamily: fonts.regular
         }}>
-                            {t("Track syllabus progress and manage notes")}
-                        </Text>}
-                </View>
+          {t("Track syllabus progress and manage notes")}
+        </Text>}
+      </View>
 
-                {isLargeScreen && <View style={{
+      {isLargeScreen && <View style={{
         width: "32%"
       }} className="justify-end flex-row">
-                        <CourseScheduleCard department={loading ? "..." : studentProfile?.department || "N/A"} degree={loading ? "..." : studentProfile?.degree || "N/A"} year={loading ? "..." : studentProfile?.year || "N/A"} style="w-[320px]" />
-          
-                    </View>}
-            </View>
+        <CourseScheduleCard department={loading ? "..." : studentProfile?.department || "N/A"} degree={loading ? "..." : studentProfile?.degree || "N/A"} year={loading ? "..." : studentProfile?.year || "N/A"} style="w-[320px]" />
 
-            <View className={isLargeScreen ? "mt-4" : ""}>
-                {loading ? <SubjectSkeleton /> : <SubjectCard subjectProps={subjects} />}
-            </View>
-        </ScrollView>;
+      </View>}
+    </View>
+
+    <View className={isLargeScreen ? "mt-4" : ""}>
+      {loading ? <SubjectSkeleton /> : <SubjectCard subjectProps={subjects} />}
+    </View>
+  </ScrollView>;
 }
 export default function StudentAcademics() {
   const {
     t
   } = useTranslation();
   return <StudentProvider>
-            <SafeAreaView edges={["left", "right", "bottom"]} className="flex-1">
-                <React.Suspense fallback={<View className="flex-1 items-center justify-center py-10">
-                            {Loader ? <Loader /> : <ActivityIndicator size="small" color="#6b7280" />}
-                            <Text className="text-sm text-gray-500 mt-2">{t("Auto.Common.LoadingAcademic", "Loading Academics...")}</Text>
-                        </View>}>
-          
-                    <AcademicsContent />
-                </React.Suspense>
-            </SafeAreaView>
-        </StudentProvider>;
+    <SafeAreaView edges={["left", "right", "bottom"]} className="flex-1">
+      <React.Suspense fallback={<View className="flex-1 items-center justify-center py-10">
+        {Loader ? <Loader /> : <ActivityIndicator size="small" color="#6b7280" />}
+        <Text className="text-sm text-gray-500 mt-2">{t("Auto.Common.LoadingAcademic", "Loading Academics...")}</Text>
+      </View>}>
+
+        <AcademicsContent />
+      </React.Suspense>
+    </SafeAreaView>
+  </StudentProvider>;
 }
